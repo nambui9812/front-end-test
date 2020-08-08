1a8e43796b06bdeae9df7987c1bcb155e7f30eab
nambui9812 <buihainam9812@gmail.com>
2020-08-07 22:04:40 -0600

    update

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import Filter from './Filter';
 
+import data from '../data.json';
+
 class Filters extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			conditions: []
+			conditions: [],
+			data: data
 		};
 	}
 
@@ -86,6 +89,61 @@ class Filters extends React.Component {
 		}));
 	}
 
+	handleFilter = () => {
+		let conditions = this.state.conditions;
+		
+		for (let i = 0; i < conditions.length; ++i) {
+			let condition = conditions[i];
+
+			console.log(condition.id);
+
+			if (condition.id === "name") {
+				this.setState({
+					data: this.state.data.filter(user => user.name.includes(condition.value))
+				})
+			}
+			else if (condition.id === "screen_name") {
+				this.setState({
+					data: this.state.data.filter(user => user.screen_name.includes(condition.value))
+				})
+			}
+			else if (condition.id === "followers_count") {
+				if (condition.operator === "GTE") {
+					this.setState({
+						data: this.state.data.filter(user => user.followers_count >= condition.value)
+					})
+				}
+				else {
+					this.setState({
+						data: this.state.data.filter(user => user.followers_count <= condition.value)
+					})
+				}
+			}
+			else if (condition.id === "following_count") {
+				if (condition.operator === "GTE") {
+					this.setState({
+						data: this.state.data.filter(user => user.following_count >= condition.value)
+					})
+				}
+				else {
+					this.setState({
+						data: this.state.data.filter(user => user.following_count <= condition.value)
+					})
+				}
+			}
+			else if (condition.id === "location") {
+				this.setState({
+					data: this.state.data.filter(user => user.location.includes(condition.value))
+				})
+			}
+			else if (condition.id === "verified") {
+				this.setState({
+					data: this.state.data.filter(user => user.verified.toString() === condition.value)
+				})
+			}
+		}
+	}
+
 	render() {
 		console.log(this.state.conditions);
 		return (
@@ -97,6 +155,36 @@ class Filters extends React.Component {
 				<div className="add-filter" onClick={() => this.handleAddFilter()}>
 					+ Add Filters
 				</div>
+
+				<button id="filter" onClick={() => this.handleFilter()}>Filter</button>
+
+				<table id="result">
+					<tbody>
+						<tr>
+							<th>Name</th>
+							<th>Screen Name</th>
+							<th>Followers Count</th>
+							<th>Following Count</th>
+							<th>Location</th>
+							<th>Verified</th>
+						</tr>
+
+						{
+							this.state.data.map((user, index) => {
+								return (
+									<tr key={index}>
+										<td>{user.name}</td>
+										<td>{user.screen_name}</td>
+										<td>{user.followers_count}</td>
+										<td>{user.following_count}</td>
+										<td>{user.location}</td>
+										<td>{user.verified ? "Yes" : "No"}</td>
+									</tr>
+								)
+							})
+						}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
