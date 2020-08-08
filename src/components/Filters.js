59941b23c6ad98c8a6b5166f05fef2664d48a367
nambui9812 <buihainam9812@gmail.com>
2020-08-08 12:28:21 -0600

    update

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -10,7 +10,7 @@ class Filters extends React.Component {
 		super(props);
 		this.state = {
 			conditions: [],
-			data: data
+			data: data,
 		};
 	}
 
@@ -91,61 +91,95 @@ class Filters extends React.Component {
 
 	handleFilter = () => {
 		let conditions = this.state.conditions;
+
+		if (conditions.length === 0) {
+			this.setState({
+				data: data
+			});
+			return;
+		}
+
+		let myData = data;
 		
+		// Go through all conditions
 		for (let i = 0; i < conditions.length; ++i) {
 			let condition = conditions[i];
 
-			console.log(condition.id);
+			// name filter
+			if (condition.id === "name" && condition.operator !== "") {
+				myData = myData.filter(user => user.name.includes(condition.value));
 
-			if (condition.id === "name") {
 				this.setState({
-					data: this.state.data.filter(user => user.name.includes(condition.value))
+					data: myData
 				})
 			}
-			else if (condition.id === "screen_name") {
+
+			// screen_name filter
+			else if (condition.id === "screen_name" && condition.operator !== "") {
+				myData = myData.filter(user => user.screen_name.includes(condition.value));
+
 				this.setState({
-					data: this.state.data.filter(user => user.screen_name.includes(condition.value))
+					data: myData
 				})
 			}
+
+			// followers_count filter
 			else if (condition.id === "followers_count") {
-				if (condition.operator === "GTE") {
+				if (condition.operator === "GTE" && !isNaN(condition.value)) {
+					myData = myData.filter(user => user.followers_count >= condition.value);
+
 					this.setState({
-						data: this.state.data.filter(user => user.followers_count >= condition.value)
+						data: myData
 					})
 				}
-				else {
+				else if(condition.operator === "LTE" && !isNaN(condition.value)) {
+					myData = myData.filter(user => user.followers_count <= condition.value);
+
 					this.setState({
-						data: this.state.data.filter(user => user.followers_count <= condition.value)
+						data: myData
 					})
 				}
 			}
+
+			// following count filter
 			else if (condition.id === "following_count") {
-				if (condition.operator === "GTE") {
+				if (condition.operator === "GTE" && !isNaN(condition.value)) {
+					myData = myData.filter(user => user.following_count >= condition.value);
+
 					this.setState({
-						data: this.state.data.filter(user => user.following_count >= condition.value)
+						data: myData
 					})
 				}
-				else {
+				else if(condition.operator === "LTE" && !isNaN(condition.value)) {
+					myData = myData.filter(user => user.following_count <= condition.value);
+
 					this.setState({
-						data: this.state.data.filter(user => user.following_count <= condition.value)
+						data: myData
 					})
 				}
 			}
-			else if (condition.id === "location") {
+
+			// location filter
+			else if (condition.id === "location" && condition.operator !== "") {
+				myData = myData.filter(user => user.location.includes(condition.value));
+
 				this.setState({
-					data: this.state.data.filter(user => user.location.includes(condition.value))
+					data: myData
 				})
 			}
-			else if (condition.id === "verified") {
+
+			// verified filter
+			else if (condition.id === "verified" && condition.operator !== "" && condition.value !== "") {
+				myData = myData.filter(user => user.verified.toString() === condition.value);
+
 				this.setState({
-					data: this.state.data.filter(user => user.verified.toString() === condition.value)
+					data: myData
 				})
 			}
 		}
 	}
 
 	render() {
-		console.log(this.state.conditions);
 		return (
 			<div className="Filters">
 				{this.state.conditions.map((condition, index) => (
